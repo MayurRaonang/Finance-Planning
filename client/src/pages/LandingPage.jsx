b1a8b821016f753e34cb5cc5c0626d1f337f44bd
MayurRaonang <mayurraonang@gmail.com>
2026-01-07 20:10:44 +0530

    made hero section footer etc component of landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,7 +1,21 @@
+import HeroSection from "../components/LandingPage/HeroSection.jsx";
+import HowItWorks from "../components/LandingPage/HowItWorks.jsx";
+import FAQSection from "../components/LandingPage/FAQSection.jsx";
+import Footer from "../components/LandingPage/Footer.jsx";
+
 const LandingPage = () => {
   return (
-    <div>
-      Landing Page
+    <div className="w-full">
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* Future sections */}
+      {/* <Features /> */}
+      <HowItWorks />
+      {/* <AIAgents /> */}
+      {/* <Pricing /> */}
+      <FAQSection />
+      <Footer />
     </div>
   );
 };
